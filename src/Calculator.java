9d890c09bd74e2afc3d53e323024b6cc6d43850a
ElizavetaKhmel <137898540+ElizavetaKhmel@users.noreply.github.com>
2023-07-25 23:43:48 +0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
-    public int calculate(int sk, double ps, int year) {
-        double kp = 12 * year;
-        double mps = ps / 100 / 12;
-        double ep = sk * (mps + (mps / (Math.pow((1 + mps), kp) - 1)));
-        return (int) ep;
+    public int calculate(int creditAmount, double precentRate, int year) {
+        double month = 12 * year;
+        double monthPrecentDate = precentRate / 100 / 12;
+        double monthPayment = creditAmount * (monthPrecentDate + (monthPrecentDate / (Math.pow((1 + monthPrecentDate), month) - 1)));
+        return (int) monthPayment;
     }
 }
